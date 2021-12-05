ec8f556effb1235e543b92638ead82aa6e42f04b
Dave Canucot <saintxcalibur@gmail.com>
2021-12-05 20:12:40 +0800

    Updates

---
+++ precache-manifest.d4e5a24342bff9c1d809610ced5d5c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8ca6750d4d5e7dfbdbd2887a68e566",
+    "url": "/Nin-Payroll/index.html"
+  },
+  {
+    "revision": "95e318ae9cdd78a341e4",
+    "url": "/Nin-Payroll/static/css/main.5d9dac51.chunk.css"
+  },
+  {
+    "revision": "cbf92142ddd64478dd4d",
+    "url": "/Nin-Payroll/static/js/2.13fcbca7.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Nin-Payroll/static/js/2.13fcbca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e318ae9cdd78a341e4",
+    "url": "/Nin-Payroll/static/js/main.3de2aaed.chunk.js"
+  },
+  {
+    "revision": "51fbee9770bff6e8a6fa",
+    "url": "/Nin-Payroll/static/js/runtime-main.604b7f5d.js"
+  }
+]);
